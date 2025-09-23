7517e5807417b95c6a772f17fc83396e20e38c21
panos <panos.polis@gmail.com>
2025-09-23 13:57:33 +0300

    - dopa remove, refactor scaling up

--- src/Models/Imet/ScalingUp/ScalingUpWdpa.php
+++ src/Models/Imet/ScalingUp/ScalingUpWdpa.php
@@ -24,30 +24,35 @@ class ScalingUpWdpa extends BaseModel
     protected $fillable = ['scaling_id', 'FormID', 'name', 'Country', 'wdpa_id', 'color'];
     public $timestamps = false;
 
+    /**
+     * @param $id
+     * @return mixed
+     */
     public static function retrieve_by_scaling_id($id)
     {
         return static::where('scaling_id', $id)->orderBy('name', 'asc')->get();
     }
 
+    /**
+     * @param $scaling_id
+     * @param $id
+     * @return mixed
+     */
     public static function getByFormID($scaling_id, $id)
     {
         return static::where(['scaling_id' => $scaling_id, 'FormID' => $id])->first();
     }
 
-    public static function save_item($item)
-    {
-        $record = static::create(['scaling_id' => $item['scaling_id']]);
-        $record->FormID = $item['wdpa_id'];
-        $record->name = $item['name'];
-        $record->save();
-        return json_encode($record);
-    }
-
-    public static function save_pas($scaling_id, $areas)
+    /**
+     * @param $scaling_id
+     * @param $areas
+     * @return array
+     */
+    public static function save_pas($scaling_id, $areas): array
     {
         $saved_pas = [];
         foreach ($areas as $k => $area) {
-            $rand_color = "#".substr(md5(rand()), 0, 6);//'rgb(' . rand(30, 220) . ',' . rand(40, 220) . ',' . rand(35, 220) . ')';//str_pad(dechex(rand(0x000000, 0xFFFFFF)), 6, 0, STR_PAD_LEFT);
+            $rand_color = "#" . substr(md5(rand()), 0, 6);//'rgb(' . rand(30, 220) . ',' . rand(40, 220) . ',' . rand(35, 220) . ')';//str_pad(dechex(rand(0x000000, 0xFFFFFF)), 6, 0, STR_PAD_LEFT);
             $saved_pas[] = static::create(['scaling_id' => $scaling_id, 'FormID' => $area->FormID, 'name' => $area->name, 'Country' => $area->Country, 'wdpa_id' => $area->wdpa_id, 'color' => $rand_color]);
         }
         return $saved_pas;
