8bd0333b9b776e110d40fb7bbfe2162eba65387e
Andrea Marelli <andrea.marelli.dev@gmail.com>
2026-01-22 14:02:32 +0100

    [Feat] Show vernacular names in animal selector

--- src/resources/views/components/inputs/selector-species.blade.php
+++ src/resources/views/components/inputs/selector-species.blade.php
@@ -1,5 +1,6 @@
 <selector-species
     {!! $vue_attributes !!}
     search-url="{{ route('imet-core::selector.species.search') }}"
+    info-url="{{ route('imet-core::selector.species.info') }}"
     :with-insert={{ Str::contains($type, 'withInsert') ? 'true' : 'false' }}
 ></selector-species>
