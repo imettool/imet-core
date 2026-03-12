0b6479506c9076c44a7bd2da6bced696754746e2
Andrea Marelli <andrea.marelli.dev@gmail.com>
2026-03-12 18:48:43 +0100

    [Chore] Refined CTX 3.3 show view

--- src/resources/views/v2/context/show/modules/equipments.blade.php
+++ src/resources/views/v2/context/show/modules/equipments.blade.php
@@ -11,39 +11,46 @@ use ImetCore\View\ScoreBar;
 use ModularForms\View\Module\Components\Field\InputPreview;
 use Wa72\HtmlPageDom\HtmlPageCrawler;
 
-
-$view_groupTable = View::make('modular-forms::module.show.type.group_table', ['definitions' => $definitions, 'records' => $records])->render();
-
-$dom = HtmlPageCrawler::create('<div class="accordion">' . $view_groupTable . '</div>');
-foreach ($definitions['groups'] as $group_key => $group) {
-
-    // Remove original group title
-    $dom->filter('div > h5.group_title_' . $definitions['module_key'] . '_' . $group_key)
-        ->remove();
-
-    // Setup accordion header and progress bar
-    $group_records = array_filter($records, fn(array $item): bool => $item['group_key'] === $group_key);
-    $group_score = round(Math::records_average($group_records, 'AdequacyLevel'), 2);
-    $header =
-        '<div class="accordion-item-header">
-            <div class="accordion-item-header-title">
-                ' . Blade::renderComponent(new ScoreBar(
-                        label: $definitions['groups'][$group_key],
-                        score: $group_score,
-                        percentage: $group_score/3*100
-                    )) . '
-            </div>
-        </div>';
-
-    $dom->filter('table#group_table_' . $definitions['module_key'] . '_' . $group_key)
-        ->prepend($header)
-        ->wrap('<div class="accordion-item show" id="group_accordion_' . $definitions['module_key'] . '_' . $group_key . '"></div>')
-        ->wrap('<div class="accordion-item-body"></div>')
-        ->wrap('<div class="accordion-item-body-content"></div>');
-
-}
+$groups = $definitions['groups'];
 
 ?>
-{!! $dom->saveHTML() !!}
+
+<div class="accordion">
+
+    @foreach($groups as $group_key => $group_label)
+
+        <div class="accordion-item show">
+
+            <div class="accordion-item-header">
+                <div class="accordion-item-header-title">
+                    @php
+                        $group_records = array_filter($records, fn(array $item): bool => $item['group_key'] === $group_key);
+                        $group_score = round(Math::records_average($group_records, 'AdequacyLevel'), 2);
+                    @endphp
+                    <x-imet-core::score-bar
+                        :label="$group_label"
+                        :score="$group_score"
+                        :percentage="$group_score/3*100"
+                    ></x-imet-core::score-bar>
+                </div>
+            </div>
+
+            <div class="accordion-item-body">
+                <div class="accordion-item-body-content">
+                    @include('modular-forms::module.show.type.table', [
+                       'definitions' => $definitions,
+                       'records' => $records,
+                       'group_key' => $group_key
+                   ])
+                </div>
+            </div>
+
+        </div>
+        <br />
+
+    @endforeach
+
+</div>
+
 
 @include('modular-forms::module.show.type.commons', ['definitions' => $definitions, 'records' => $records])
