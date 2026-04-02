458b4c52b09b7ae35cfff1e86d329d77f126a82d
Andrea Marelli <andrea.marelli.dev@gmail.com>
2026-04-02 16:32:53 +0200

    [Fix] Fix total calculations in FinancialAvailableResources

--- src/resources/views/v2/context/edit/modules/financial_available_resources.blade.php
+++ src/resources/views/v2/context/edit/modules/financial_available_resources.blade.php
@@ -15,85 +15,92 @@ $table_id = 'table_' . $definitions['slug'];
 
     {{-- labels  --}}
     <thead>
-    <tr>
-        @foreach($definitions['fields'] as $field)
-            @if($field['type']!=='hidden')
-                <th class="text-center">
-                    {{ ucfirst($field['label'] ?? '') }}
-                </th>
-            @endif
-        @endforeach
-        <th class="text-center">
-            @uclang('imet-core::v2_context.FinancialAvailableResources.fields.total')
-        </th>
-        <th class="text-center">
-            @uclang('imet-core::v2_context.FinancialAvailableResources.fields.percentage')
-        </th>
-    </tr>
+        <tr>
+            @foreach($definitions['fields'] as $field)
+                @if($field['type']!=='hidden')
+                    <th class="text-center">
+                        {{ ucfirst($field['label'] ?? '') }}
+                    </th>
+                @endif
+            @endforeach
+            <th class="text-center">
+                @uclang('imet-core::v2_context.FinancialAvailableResources.fields.total')
+            </th>
+            <th class="text-center">
+                @uclang('imet-core::v2_context.FinancialAvailableResources.fields.percentage')
+            </th>
+        </tr>
     </thead>
 
     {{-- inputs --}}
     <tbody>
-    <tr class="module-table-item" v-for="(item, index) in records">
-        {{--  fields  --}}
-        @foreach($definitions['fields'] as $field)
+        <tr class="module-table-item" v-for="(item, index) in records">
+            {{--  fields  --}}
+            @foreach($definitions['fields'] as $field)
+                <td>
+                    @include('modular-forms::module.edit.field.module-to-vue', [
+                        'definitions' => $definitions,
+                        'field' => $field,
+                        'vue_record_index' => 'index',
+                        'group_key' => $group_key
+                    ])
+                </td>
+            @endforeach
+
             <td>
-                @include('modular-forms::module.edit.field.module-to-vue', [
-                    'definitions' => $definitions,
-                    'field' => $field,
-                    'vue_record_index' => 'index',
-                    'group_key' => $group_key
-                ])
-            </td>
-        @endforeach
-        <td>
-            {{-- budget line total --}}
-            <input type="numeric" disabled="disabled"
-                   class="field-edit field-numeric text-right"
-                   v-bind:value="totals[index]"
-                   v-bind:id="'{{$definitions['slug'] }}_'+index+'_total'"
-            />
-        </td>
-        <td>
-            {{-- percentage of planned budget --}}
-            <input type="text" disabled="disabled" style="width: 80px;"
-                   class="field-edit field-numeric text-center"
-                   v-bind:value="percentages[index]"
-                   v-bind:id="'{{$definitions['slug'] }}_'+index+'_percentage'"
-            />
-        </td>
-        <td>
-            {{-- record id  --}}
-            <x-imet-core::custom-input
-                    type="hidden"
-                    :value="'item.'.$definitions['primary_key']"
-            ></x-imet-core::custom-input>
-            <span v-if="typeof item.__predefined === 'undefined'">
-                <x-modular-forms::module.components.buttons.delete-item/>
-            </span>
-        </td>
-    <tr>
-
-
-    <tr>
-        <td colspan="5">
-            {{-- not valid total budget (error message)  --}}
-            <div v-if="!totalIsValid" class="text-contextual-danger text-right" style="font-size: 0.9em;">
-                <i class="fa fa-exclamation-triangle"></i>
-                {!!  ucfirst(trans('imet-core::v2_context.FinancialAvailableResources.sum_error')) !!}
-            </div>
-        </td>
-        <td>
-            {{-- total budget --}}
-            <div :class="!totalIsValid ? 'has-error' : 'form-group'">
-                <input type="text" disabled="disabled"
-                       class="field-edit field-numeric text-center"
-                       v-bind:value="sumTotals"
+                {{-- budget line total --}}
+                <input type="numeric" disabled="disabled"
+                       class="field-edit field-numeric text-right"
+                       v-bind:value="totals[index]"
+                       v-bind:id="'{{$definitions['slug'] }}_'+index+'_total'"
+                       v-if="index!==0"
                 />
-            </div>
-        </td>
-        <td></td>
-    </tr>
+            </td>
+
+            <td>
+                {{-- percentage of planned budget --}}
+                <input type="text" disabled="disabled" style="width: 80px;"
+                       class="field-edit field-numeric text-center"
+                       v-bind:value="percentages[index]"
+                       v-bind:id="'{{$definitions['slug'] }}_'+index+'_percentage'"
+                       v-if="index!==0"
+                />
+            </td>
+            <td>
+                {{-- record id  --}}
+                <x-imet-core::custom-input
+                        type="hidden"
+                        :value="'item.'.$definitions['primary_key']"
+                ></x-imet-core::custom-input>
+                <span v-if="typeof item.__predefined === 'undefined'">
+                    <x-modular-forms::module.components.buttons.delete-item/>
+                </span>
+            </td>
+        <tr>
+
+
+        {{-- TOTAL calulation --}}
+        <tr>
+            <td colspan="5">
+                {{-- not valid total budget (error message)  --}}
+                <div v-if="!totalIsValid" class="text-contextual-danger text-right mr-4" style="font-size: 0.9em;">
+                    <i class="fa fa-exclamation-triangle"></i>
+                    {!!  ucfirst(trans('imet-core::v2_context.FinancialAvailableResources.sum_error')) !!}
+                </div>
+            </td>
+            <td>
+                {{-- total budget --}}
+                <div :class="!totalIsValid ? 'has-error' : 'form-group'">
+                    <input type="text" disabled="disabled"
+                           class="field-edit field-numeric text-center"
+                           v-bind:value="sumTotals"
+                    />
+                </div>
+            </td>
+            <td></td>
+        </tr>
+
+
     </tbody>
 
 </table>
