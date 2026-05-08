10a49f80f16b0c626ea4ae9b9ae3e2d00288df7d
Andrea Marelli <andrea.marelli.dev@gmail.com>
2026-05-08 14:17:32 +0200

    [fix] fix issue with null legend

--- src/resources/views/components/inputs/rating.blade.php
+++ src/resources/views/components/inputs/rating.blade.php
@@ -8,6 +8,8 @@ $legend = new $module_class()->ratingLegend[$field_name] ?? null;
 
 <rating
     rating-type="{{ str_replace('rating-', '', $type) }}"
-    :legend='@json(array_values($legend))'
+    @if($legend!==null)
+        :legend='@json(array_values($legend))'
+    @endif
     {!! $vue_attributes !!} data-{!! $class_attribute !!} {!! $rules_attribute !!} {!! $other_attributes !!}
 ></rating>
