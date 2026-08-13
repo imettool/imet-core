94837800a628cabc7875ec9f67e8ae13bcdd4f8a
Andrea Marelli <andrea.marelli.dev@gmail.com>
2026-08-13 12:08:19 +0200

    [feat] Add kce names to RP 4B header

--- src/resources/views/v2/report/edit/modules/threats_affecting_kces.blade.php
+++ src/resources/views/v2/report/edit/modules/threats_affecting_kces.blade.php
@@ -6,6 +6,14 @@
 
 use ImetCore\Models\Imet\Components\Modules\ImetModule;
 
+$kce_idx = 0;
+foreach ($definitions['fields'] as $i => $field) {
+    if (\Illuminate\Support\Str::startsWith($field['name'], 'kce')) {
+        $definitions['fields'][$i]['label'] = $field['label'] . ' <div class="italic" v-html=kce_names[' . $kce_idx . ']></div>';
+        $kce_idx++;
+    }
+}
+
 ?>
 
 @include('modular-forms::module.edit.type.table', ['definitions' => $definitions])
